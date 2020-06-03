7fb7c5fa22f023970f10f8f26f6a320cc44d6a56
Karo <karoline@familie-rabe.de>
2020-06-03 09:01:08 +0200

    API call with restTemplate and Bean injection

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.kt
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.kt
@@ -3,19 +3,22 @@ package io.javabrains.moviecatalogservice.resources
 import io.javabrains.moviecatalogservice.modules.CatalogItem
 import io.javabrains.moviecatalogservice.modules.Movie
 import io.javabrains.moviecatalogservice.modules.Rating
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.getForObject
 
 @RestController
 @RequestMapping("/catalog")
 class MovieCatalogResource {
+
+    @Autowired
+    private lateinit var restTemplate : RestTemplate
+
     @RequestMapping("/{userId}")
     fun getCatalog(@PathVariable userId: String) : List<CatalogItem> {
 
-        val restTemplate = RestTemplate()
         val ratings = listOf(Rating("123", 4), Rating("456", 6))
         return ratings.map { rating ->
             val movie = requireNotNull(restTemplate.getForObject("http://localhost:8081/movie/${rating.movieId}", Movie::class.java))
