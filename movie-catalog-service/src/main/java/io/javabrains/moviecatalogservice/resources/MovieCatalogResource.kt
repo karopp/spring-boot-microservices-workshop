3bc79533f3f85924650e27d7dabf542867a543fa
Karo <karoline@familie-rabe.de>
2020-06-03 09:31:57 +0200

    Replace restTemplate by webClient

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.kt
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.kt
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 
 @RestController
 @RequestMapping("/catalog")
@@ -16,13 +18,26 @@ class MovieCatalogResource {
     @Autowired
     private lateinit var restTemplate : RestTemplate
 
+    @Autowired
+    private lateinit var webClientBuilder : WebClient.Builder
+
     @RequestMapping("/{userId}")
     fun getCatalog(@PathVariable userId: String) : List<CatalogItem> {
 
         val ratings = listOf(Rating("123", 4), Rating("456", 6))
+
         return ratings.map { rating ->
-            val movie = requireNotNull(restTemplate.getForObject("http://localhost:8081/movie/${rating.movieId}", Movie::class.java))
-             CatalogItem(movie.name, "Action", rating.rating) }.toList()
+            val movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8081/movie/${rating.movieId}")
+                    .retrieve()
+                    .bodyToMono(Movie::class.java)
+                    .block()
+            CatalogItem(movie.name, "Action", rating.rating) }.toList()
+
+        //return ratings.map { rating ->
+        //    val movie = requireNotNull(restTemplate.getForObject("http://localhost:8081/movie/${rating.movieId}", Movie::class.java))
+        //     CatalogItem(movie.name, "Action", rating.rating) }.toList()
 
 
 
